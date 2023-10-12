a3d2420b479d6fae4dd0812f6da91f0a018154c6
113914-MolinaTorres-Julian <113914@tecnicatura.frc.utn.edu.ar>
2023-10-12 15:17:30 -0300

    Template CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from "./models/cliente";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CRUD';
+
+  clientes: Cliente[] = [
+    {id: 1, nombre: "Matías", apellido: "Lokman", dni: 12345, numTelefono: 6789},
+    {id: 2, nombre: "Gustavo", apellido: "Olivera", dni: 12345, numTelefono: 6789},
+    {id: 3, nombre: "Victorio", apellido: "Perez", dni: 12345, numTelefono: 6789}
+  ];
+
+  selectedCliente: Cliente = new Cliente();
+
+  agregarCliente() {
+    this.selectedCliente.id = this.clientes.length + 1;
+    this.clientes.push(this.selectedCliente);
+
+    this.selectedCliente = new Cliente();
+  }
+
+  editarCliente() {
+
+  }
+
+  borrarCliente() {
+    if(confirm("¿Realmente desea borrar al cliente seleccionado?")) {
+      this.clientes = this.clientes.filter(x => x != this.selectedCliente);
+      this.selectedCliente = new Cliente();
+    }
+  }
+
+  llenarCampos(cliente: Cliente) {
+    this.selectedCliente = cliente;
+  }
+
 }
---
+++ src/app/models/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+    id: number = 0;
+    nombre: string = "";
+    apellido: string = "";
+    dni: number = 0;
+    numTelefono: number = 0;
+}
